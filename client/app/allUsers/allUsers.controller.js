51d70418a9182885fb4305ef15c288060419f5eb
Una Gauper <umgauper@gmail.com>
2015-09-30 17:27:21 -0700

    added highlight style to the clicked user name

--- client/app/allUsers/allUsers.controller.js
+++ client/app/allUsers/allUsers.controller.js
@@ -2,15 +2,21 @@
 
 angular.module('linkterestApp')
   .controller('AllUsersCtrl', function ($scope, $http, Auth) {
+
+    $scope.currentUser = Auth.getCurrentUser().name;
+
     $http.get('/api/users').success(function(data) {
       $scope.allUsers = data;
     });
-
-    $scope.currentUser = Auth.getCurrentUser().name;
 
     $scope.loadImages = function(_id) {
       $http.get('/api/images/owner/' + _id).success(function (data) {
         $scope.bricks = data;
       });
-    }
+    };
+
+    $scope.setActive = function(name) {
+      $scope.active = name;
+    };
+
   });
